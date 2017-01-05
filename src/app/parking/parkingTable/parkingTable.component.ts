430d8a2ed7d6d66f501336c28190e9ac3fef6bc7
sezarmbar <sezar_mbar@yahoo.com>
2017-01-05 15:43:12 +0100

    saple

--- src/app/parking/parkingTable/parkingTable.component.ts
+++ src/app/parking/parkingTable/parkingTable.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit , ViewChild, ViewContainerRef } from '@angular/core';
 import {MdSidenav, MdDialog, MdDialogConfig} from "@angular/material";
 import { ParkingsService } from '../service/parking.service';
 import {Observable} from 'rxjs/Rx';
+
+
+
 @Component({
   selector: 'app-parkingTable',
   templateUrl: './parkingTable.component.html',
@@ -11,28 +14,30 @@ export class ParkingTableComponent implements OnInit {
 
 @ViewChild('sidenav') sidenav: MdSidenav;
 
- parkings:any;
+ Parkhaus:any;
  lastTime:any;
+ currentPark={};
  private subscription;
   constructor(private service:ParkingsService) {
   }
 
   ngOnInit() {
-    this.subscription = Observable.interval(2000 * 60 ).subscribe(x => {
+    this.callService() ;
+    this.subscription = Observable.interval(2000).subscribe(x => {
     this.callService();
   });
     
   }
-showParking() {
+showPark(park) {
+    this.currentPark=park;
     this.sidenav.open();
   }
 
   callService() {
     this.service.getParks().subscribe(
       (parkings) => {
-        this.parkings = parkings.Daten.Parkhaus;
-        this.lastTime = parkings.Daten.Zeitstempel;
-        console.log(this.lastTime);
+        this.Parkhaus = parkings.Daten.Parkhaus;
+        this.lastTime = parkings.Daten.Zeitstempel;  
       });
   }
    ngOnDestroy() {
