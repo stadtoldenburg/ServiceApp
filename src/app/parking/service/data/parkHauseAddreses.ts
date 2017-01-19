92b30a62a2a8e53af7cb2516ffe58a7c3cff6d06
sezarmbar <sezar_mbar@yahoo.com>
2017-01-19 12:51:38 +0100

    fix  contant

--- src/app/parking/service/data/parkHauseAddreses.ts
+++ src/app/parking/service/data/parkHauseAddreses.ts
@@ -6,7 +6,7 @@
     {name: 'Pferdemarkt', lat: 53.1451262 ,lng:8.2118626 },
     {name: 'CCO Parkdeck 1', lat: 53.1416318 ,lng: 8.2113736 },
     {name: 'CCO Parkdeck 2', lat: 53.1445094 ,lng:8.2127458 },
-    {name: 'Hbf/ZOB', lat: 53.1432439, lng:8.2214212},
+    {name: 'Hbf/ZOB', lat: 53.1449202, lng:8.2232335},
     {name: 'Theaterwall', lat: 53.1398954 ,lng: 8.2088435 },
     {name: 'Theatergarage', lat: 53.1398954 ,lng:8.2088435 },
     {name: 'Heiligengeist-Hoefe', lat: 53.1447143 ,lng:8.2091045 },
