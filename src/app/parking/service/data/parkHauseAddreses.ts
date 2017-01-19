1e837d5fdd81069441d92333fbefc7e8c95d2bdd
sezarmbar <sezar_mbar@yahoo.com>
2017-01-19 07:54:57 +0100

    addreses data

--- src/app/parking/service/data/parkHauseAddreses.ts
+++ src/app/parking/service/data/parkHauseAddreses.ts
@@ -1,14 +1,14 @@
 
   export const ADDRESES= [
-    {name: 'Waffenplatz', lat: 53.1432439 ,lng:8.2157246},
+    {name: 'Waffenplatz', lat: 53.1417746 ,lng:8.2102227},
     {name: 'City', lat: 53.1432439 ,lng:8.2157246 },
-    {name: 'Galeria Kaufhof', lat: 53.1432439 ,lng:8.2157246 },
-    {name: 'Pferdemarkt', lat: 53.1432439 ,lng:8.2157246 },
-    {name: 'CCO Parkdeck 1', lat: 53.1443608 ,lng: 8.2157246 },
-    {name: 'CCO Parkdeck 2', lat: 53.1432439 ,lng:8.2157246 },
+    {name: 'Galeria Kaufhof', lat: 53.1389029 ,lng:8.2142872 },
+    {name: 'Pferdemarkt', lat: 53.1451262 ,lng:8.2118626 },
+    {name: 'CCO Parkdeck 1', lat: 53.1416318 ,lng: 8.2113736 },
+    {name: 'CCO Parkdeck 2', lat: 53.1445094 ,lng:8.2127458 },
     {name: 'Hbf/ZOB', lat: 53.1432439, lng:8.2214212},
-    {name: 'Theaterwall', lat: 53.1432439 ,lng:8.2157246 },
-    {name: 'Theatergarage', lat: 53.1432439 ,lng:8.2157246 },
-    {name: 'Heiligengeist-Hoefe', lat: 53.1432439 ,lng:8.2157246 },
-    {name: 'Schlosshoefe', lat: 53.1432439 ,lng:8.2157246 },
+    {name: 'Theaterwall', lat: 53.1398954 ,lng: 8.2088435 },
+    {name: 'Theatergarage', lat: 53.1398954 ,lng:8.2088435 },
+    {name: 'Heiligengeist-Hoefe', lat: 53.1447143 ,lng:8.2091045 },
+    {name: 'Schlosshoefe', lat: 53.138548 ,lng:8.2144813 },
   ];
